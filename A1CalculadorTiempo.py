76669dbe55ffbde440fd1a766d0e67a921f94651
MkCst <mikelopezcastillo@gmail.com>
2021-07-13 00:26:26 -0500

    ejercicio completado

--- A1CalculadorTiempo.py
+++ A1CalculadorTiempo.py
@@ -36,8 +36,7 @@ def add_time(start, duration, day_of_week=False):
 
     # CANTIDAD DE DIAS TRANSCURRIDOS
     amount_days = duration_hours // 24
-    print("Dias transcurridos: ", amount_days)
-
+    
     # Minutos establecidos
     minutes_assigned = start_minutes + duration_minutes
     # Asignando horas y minutos
@@ -45,28 +44,40 @@ def add_time(start, duration, day_of_week=False):
         start_hours += 1
         minutes_assigned = minutes_assigned % 60
     iteration_ampmCount = (start_hours + duration_hours) // 12
-
     hours_assignated = (start_hours + duration_hours) % 12
     print("Horas asignadas", hours_assignated)
     print("Minutos asignados", minutes_assigned)
-
-    # ESTABLECIENDO HORARIO
-    period_day = set_period[period_day] if iteration_ampmCount % 2 == 1 else period_day
 
     # APLICANDO FORMATO
     minutes_assigned = minutes_assigned if minutes_assigned > 9 else "0" + \
         str(minutes_assigned)
     hours_assignated = hours_assignated = 12 if hours_assignated == 0 else hours_assignated
+    if(period_day=="PM" and start_hours +(duration_hours%12)>=12):
+       amount_days+=1
     print(hours_assignated, minutes_assigned, period_day)
-
-    # FORMATO DE REGRESO
-    result_time = str(hours_assignated) + ":"+str(minutes_assigned) + " "+period_day
+    print("numero de dias:", amount_days)
+    
+    # ESTABLECIENDO HORARIO
+    period_day = set_period[period_day] if iteration_ampmCount % 2 == 1 else period_day
+    # ESTABLECIENDO DIA
     if (day_of_week):
         day_of_week = day_of_week.lower()
         index = int((day_of_week_index[day_of_week]) + amount_days) % 7
         newday = day_of_week_array[index]
-        result_time+=", "+newday
-    return result_time
+
+
+    # FORMATO DE REGRESO
+    result_time = str(hours_assignated) + ":" + str(minutes_assigned) + " "+period_day
+    days_later = "(next day)" if amount_days == 1 else "(" + str(amount_days)+" days later)"
+    
+    if (amount_days >= 1 and day_of_week==False):
+        result_time +=" "+days_later
+    if (amount_days>=1 and day_of_week):
+        result_time+= ", "+newday+" "+days_later
+    if (amount_days==0 and day_of_week):
+        result_time+= ", "+newday
+    return result_time 
+
 
 # Simulacion de main
-print(add_time("11:30 AM", "2:32", "Monday" ))
+print(add_time("6:30 PM","205:12"))
