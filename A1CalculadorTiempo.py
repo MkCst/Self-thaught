f395e0d6b52a537506a78555d4a5fc97f5e9263d
MkCst <mikelopezcastillo@gmail.com>
2021-07-12 22:41:31 -0500

    Se obtiene el archivo final, rellenar formato

--- A1CalculadorTiempo.py
+++ A1CalculadorTiempo.py
@@ -12,7 +12,7 @@ def add_time(start, duration, day_of_week=False):
         "Monday", "Tuesday",
         "Wednesday", "Thursday",
         "Friday", "Saturday", "Sunday"]
-    
+
     # DURACION
     # Definicion
     duration_tuple = duration.partition(":")
@@ -32,7 +32,7 @@ def add_time(start, duration, day_of_week=False):
 
     # PERIODO DEL DIA
     period_day = start_2do_arg[2]
-    period_dayDict = {"AM": "PM", "PM": "AM"}
+    set_period = {"AM": "PM", "PM": "AM"}
 
     # CANTIDAD DE DIAS TRANSCURRIDOS
     amount_days = duration_hours // 24
@@ -40,16 +40,33 @@ def add_time(start, duration, day_of_week=False):
 
     # Minutos establecidos
     minutes_assigned = start_minutes + duration_minutes
-    # Asignando horas
+    # Asignando horas y minutos
     if(minutes_assigned >= 60):
         start_hours += 1
         minutes_assigned = minutes_assigned % 60
     iteration_ampmCount = (start_hours + duration_hours) // 12
-    
+
     hours_assignated = (start_hours + duration_hours) % 12
-    print("Horas asignadas",hours_assignated)
+    print("Horas asignadas", hours_assignated)
     print("Minutos asignados", minutes_assigned)
 
+    # ESTABLECIENDO HORARIO
+    period_day = set_period[period_day] if iteration_ampmCount % 2 == 1 else period_day
+
+    # APLICANDO FORMATO
+    minutes_assigned = minutes_assigned if minutes_assigned > 9 else "0" + \
+        str(minutes_assigned)
+    hours_assignated = hours_assignated = 12 if hours_assignated == 0 else hours_assignated
+    print(hours_assignated, minutes_assigned, period_day)
+
+    # FORMATO DE REGRESO
+    result_time = str(hours_assignated) + ":"+str(minutes_assigned) + " "+period_day
+    if (day_of_week):
+        day_of_week = day_of_week.lower()
+        index = int((day_of_week_index[day_of_week]) + amount_days) % 7
+        newday = day_of_week_array[index]
+        result_time+=", "+newday
+    return result_time
 
 # Simulacion de main
-print(add_time("1:00 AM", "10:59", "Monday"))
+print(add_time("11:30 AM", "2:32", "Monday" ))
