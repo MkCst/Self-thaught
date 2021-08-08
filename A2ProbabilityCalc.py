754a72d131797218e318484cd47548b040e8973d
MkCst <mikelopezcastillo@gmail.com>
2021-08-08 16:47:24 -0500

    Modificaciones antes de correccion

--- A2ProbabilityCalc.py
+++ A2ProbabilityCalc.py
@@ -29,30 +29,31 @@ class Hat:
         
 
 # EXPERIMENTO PRINCIPAL
-def experiment(hat, expected_balls, nBallsDraw, nExperiments):
-    if nBallsDraw > hat.nBalls:
-        nBallsDraw = hat.nBalls
-
-    experiments = [random.sample(hat.contents, nBallsDraw) for _ in range(0, nExperiments)] 
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    if num_balls_drawn > hat.nBalls:
+        num_balls_drawn = hat.nBalls
+    
+    experiments = [random.sample(hat.contents, num_balls_drawn) for _ in range(0, num_experiments)] 
     counter_experiments = [Counter(i) for i in experiments]
 
-    nSuccess = 0
+    n_success = 0
     for i in counter_experiments:
         bool_success = True
         for k,v in expected_balls.items():
-            if v>i[k]:
+            if v > i[k]:
                 bool_success = False
                 break
             if bool_success:
-                nSuccess +=1
-    return nSuccess / nExperiments
+                n_success +=1
+    return n_success / num_experiments
+
 
 if __name__ == '__main__':
-    hat = Hat(blue=4, red=2, green=6)
+    hat = Hat(blue=4, red=2, green=6)   
     probability = experiment(
         hat=hat,
         expected_balls={"blue":2, "red":1},
-        nBallsDraw=4,
-        nExperiments=3000
+        num_balls_drawn=4,
+        num_experiments=3000
     )
     print("La probabilidad es: ",probability)
